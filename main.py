2df22b81248e40f52f971ac750b665fc604bf2ef
AleksandraIvahn <91884342+AleksandraIvahno@users.noreply.github.com>
2022-01-17 12:55:07 +0000

    2 to divi

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-teksts = input("Ievadiet skaitli:")
-def countnumbers (teksts): 
-  summa = 0
-  for simbols in teksts:
-    summa = summa + int(simbols)
-  print(summa)
-  return summa
-countnumbers (teksts)
+teksts = input ("Ievadiet skaitli: ")
+def replaceTwos(teksts):
+  if teksts.count("2")>0:
+    teksts = teksts.replace("2","divi")
+    print(teksts)
+  else:
+   teksts = "Nekas netiska aizvietuts"
+   print (teksts)
+  return teksts
+replaceTwos(teksts)
